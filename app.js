40f2bf1024140cdc164b049a424f6216b28ce686
Suleiman Mustapha <suleimamusty@gmail.com>
2022-11-28 06:55:14 +0100

    app.js

--- app.js
+++ app.js
@@ -4,7 +4,33 @@ const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
 const choice = options[Math.floor(Math.random() * options.length)]
-console.log(choice)
+return choice;
 }
 
-getComputerChoice();
+function checkWinner(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection){
+        return "Tie";
+    } else if ((playerSelection == "rock" && computerSelection == "scissors")||
+    (playerSelection == "paper" && computerSelection == "rock")||
+    (playerSelection == "scissors" && computerSelection == "paper")){
+        return "Player";
+    } else {
+        return "Computer";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+   console.log("Begin");
+   const result = checkWinner(playerSelection, computerSelection);
+      if (result == "Tie"){
+        return "It is a draw";
+      } else if (result == "Player"){
+        return `You Won! ${playerSelection} beats ${computerSelection}`;
+      } else {
+        return `You Lost! ${computerSelection} beats ${playerSelection}`;
+      }
+   }
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
